4b0bbc820510c74bcc224402e05d4cbacecb5922
Anand Bhosale <anand.bhosale@bookmyshow.com>
2018-07-04 19:44:02 +0530

    Added check for role part

--- model/user.js
+++ model/user.js
@@ -5,8 +5,11 @@ module.exports = {
   async checkUser(data) {
     const checkQuery =  { $or: [{email: data.email}, {_id : objectID(data.userId) }]  };
 
-    if(data.role) {
+    if (data.role && Array.isArray(data.role)) {
       checkQuery['$or'][0].role = {$in: data.role};
+    }
+    else {
+      checkQuery['$or'][0].role = data.role.toUpperCase();
     }
 
     const response = await mongoClient.collection(userDB).findOne(checkQuery);
